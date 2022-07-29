6cb2cd175406b8bfee86c6483097e65d0736b37e
rmves <rvesleda@gmail.com>
2022-07-28 21:54:53 -0500

    added game board methods to keep track of moves

--- main.js
+++ main.js
@@ -3,19 +3,79 @@
 const gameBoard = (() => {
      const board = ['','','','','','','','','']
 
+     const setField = (index,sign) => {
+        if (index > index.board.length) return;
+        board[index] = sign;
+     };
+
+     const getField = (index) => {
+        if (index > index.board.length) return;
+        return board[index] = sign;
+     };
+
+     const reset = () => {
+        for (let i of board) {
+            board[i] = '';
+        }
+     };
+     return { getField, setField, reset };
 })();
 
-const Player = () => {
+const Player = (sign) => {
+    this.sign = sign
 
+    const getSign = () => {
+        return sign
+    }
+
+    return { getSign }
 }
+
 
 const displayController = (() => {
     const cells = document.querySelectorAll('.field')
     const message = document.querySelector('#message')
     const resetBtn = document.querySelector('#restart-button')
+    const playerTurnMessage = document.querySelector('.message')
 
-    cells.forEach(cell => cell.addEventListener('click',(e)=> {
-        if (e.target.textContent !== '') return
-        
-    }))
+    const playerTurn = (() => {
+        const playerTurnMessage = document.querySelector('.message')
+        let firstToAct = [Player('X').getSign(),Player('O').getSign()][Math.round(Math.random())]
+        playerTurnMessage.textContent = `Player ${firstToAct}'s Turn`
+        cells.forEach(cell => cell.addEventListener('click',(e)=> {
+            if (e.target.textContent !== '') return
+            if (firstToAct === 'X') {
+                e.target.textContent = 'X'
+                firstToAct = 'O'
+                playerTurnMessage.textContent = `Player ${firstToAct}'s Turn`
+            } 
+            else { 
+                e.target.textContent = 'O'
+                firstToAct='X'
+                playerTurnMessage.textContent = `Player ${firstToAct}'s Turn`
+            }
+        }))
+    })()
+})();
+
+
+const gamePlay = (() => {
+    const playerX = Player('X')
+    const playerO = Player('O')
+    let round = 1
+    let gameOver = false
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    
+
 })();
